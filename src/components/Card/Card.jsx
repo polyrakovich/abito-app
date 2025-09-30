c6dbf435fc9f34f79ae8f36ebc164568f8cb7a53
polina <polina.rakovich23@gmail.com>
2025-09-30 14:19:31 +0300

    abito

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Card.css'
+
+const Card = ({title, price, address, date, img}) => {
+    return (
+        <a href="/product.html" className="card">
+            <div className="card--img">
+                <img src={img} alt="card-img"/>
+            </div>
+
+            <h5 className="card--title">
+                {title}
+            </h5>
+
+            <strong className="card--price">{price}</strong>
+
+            <div className="card--desc-box">
+                <span className="card--desc">{address}</span>
+                <span className="card--desc">{date}</span>
+            </div>
+        </a>
+    );
+};
+
+export default Card;
